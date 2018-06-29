7dcabb69669c014263264488ba0aaa6bfbc8f99f
martinbobbio1@gmail.com <mbobbio@varcelona.com>
2018-06-29 18:28:43 -0300

    inicio pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+//ANGULAR
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
+//COMPONENTES
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
